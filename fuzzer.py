74e7212fb349ac42be5ce2c6f718855214103a4b
Flex2424 <fakestratocaster@yandex.ru>
2017-03-05 23:35:18 +0300

    added class-fuzzer

---
+++ fuzzer.py
@@ -0,0 +1,27 @@
+import subprocess
+from sys import argv, exit
+import binascii
+
+
+class ConfigFuzzer:
+    def __init__(self, configfile, binaryfile):
+        self.config_name = configfile
+        self.file_name = binaryfile
+
+    def execute_program(self):
+        proc = subprocess.Popen([self.file_name], stdout=subprocess.PIPE, shell=True)
+        (out, err) = proc.communicate()
+        return out, err
+
+    def fuzzing(self):
+        pass
+
+
+if len(argv) == 3:
+    config = argv[1]
+    file = argv[2]
+    obj = ConfigFuzzer(config, file)
+    obj.fuzzing()
+else:
+    print("Usage: ./script.py config file")
+    exit(-1)
